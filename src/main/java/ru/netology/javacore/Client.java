d7c2f670ea4cab5aca9cd54871f3ae57dce85be4
vsuyazov <suiazov.vlad@gmail.com>
2022-07-20 19:41:24 +0300

    Change port commit

--- src/main/java/ru/netology/javacore/Client.java
+++ src/main/java/ru/netology/javacore/Client.java
@@ -15,7 +15,7 @@ public class Client {
 
     public static void main(String[] args) throws IOException {
         try (
-                Socket socket = new Socket("localhost", 8980);
+                Socket socket = new Socket("localhost", 8989);
                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true)
         ) {
--- src/main/java/ru/netology/javacore/TodoServer.java
+++ src/main/java/ru/netology/javacore/TodoServer.java
@@ -12,8 +12,7 @@ public class TodoServer {
                 try (
                         Socket socket = serverSocket.accept();
                         BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                        PrintWriter out = new PrintWriter(socket.getOutputStream(), true))
-                {
+                        PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
                     final String json = in.readLine();
                     todos.setTasks(json, todos);
                     for (String t : todos.getAllTasks()
@@ -31,7 +30,7 @@ public class TodoServer {
     }
 
     public void start() throws IOException {
-        int port = 8980;
+        int port = 8989;
         System.out.println("Starting server at " + port + "...");
     }
 }
